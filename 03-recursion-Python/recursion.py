6136db93513b77974fff8c8289d9cb6471047709
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-15 17:51:02 +0530

    Jul 15  17:51:02

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -4,7 +4,7 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
-    if position < 1:
+    if position <= 1:
         return position
     else:
         return (get_fib(position - 1) + get_fib(position - 2))
