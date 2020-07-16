a88dd49395c4954dba86992abdde77ee99f823fa
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-16 22:04:46 +0530

    Jul 16  22:04:46

--- 03-nth_fibonacci-Python/nth_fibonacci.py
+++ 03-nth_fibonacci-Python/nth_fibonacci.py
@@ -8,5 +8,10 @@
 
 
 def fun_nthfibonaccinumber(n):
-	# your code goes Here
-	return 0
+	if n==1:
+		return 0
+	elif n == 2:
+		return 1
+	else:
+		return fun_nthfibonaccinumber(n - 1) + fun_nthfibonaccinumber(n-2)
+		
