63d80af3738527f0f065dc903b1a4eb1320e1f4c
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-17 00:55:13 +0530

    Jul 17  00:55:13

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -10,12 +10,3 @@ def mostfrequentdigit(n):
 	l = (dict(Counter(n)))
 	max_key = max(l, key = l.get)
 	return max_key	
-
-print(mostfrequentdigit(5231123123123))
-print(mostfrequentdigit(5312312355565))
-print(mostfrequentdigit(2))
-print(mostfrequentdigit(24))
-print(mostfrequentdigit(1102300))
-print(mostfrequentdigit(26011))
-
-
