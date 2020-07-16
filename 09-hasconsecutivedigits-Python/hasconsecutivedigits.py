55926b7149af40fc015e6193d1365e28344605ac
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-17 00:03:58 +0530

    Jul 17  00:03:58

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -10,7 +10,7 @@ def hasconsecutivedigits(n):
 		n = abs(n)
 	lst = [elem for elem in str(n)]
 	
-	for i in range(1, len(lst)+1):
+	for i in range(len(lst)):
 		if (len(lst) == 1):
 			return False
 		if (lst[i]) == (lst[i+1]):
