9478bb4a7ed4084a82ccfe250ddb759e50e7a057
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-18 01:29:01 +0530

    Jul 18  01:29:01

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -9,7 +9,7 @@ def issorted(a):
 	if len(a) == 0 or len(a) == 1:
 		return True
 	for i in range(len(a)):
-		if a[i]<=a[i+1] or a[i]>a[i+1]:
+		if a[i]<=a[i+1] or a[i]>=a[i+1]:
 			return True
 		else:
 			return False
