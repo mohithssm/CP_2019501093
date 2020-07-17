3ffb9947421c5ea50241df051d6c5cba982d46c0
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-18 01:55:31 +0530

    Jul 18  01:55:31

--- 12-matrixadd-Python/matrixadd.py
+++ 12-matrixadd-Python/matrixadd.py
@@ -16,5 +16,7 @@
 # cannot be added because they are of different dimensions.
 
 def matrixadd(L, M):
-	# Your code goes here
-	pass
+	for i in range(len(L)):
+		for j in range(len(M)):
+			result[i][j] = L[i][j] + M[i][j]
+	return result
