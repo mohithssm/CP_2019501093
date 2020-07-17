1ea18832b67961e90b7ae05708809ed5bdcb8ceb
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-17 22:52:34 +0530

    Jul 17  22:52:34

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,34 @@
 
 
 
+def fun_nth_happy_number(n):
+	count = 1
+	while n>= 0:
+		if ishappynumber(count) and isprimeNum(count):
+			n = n - 1
+		count = count + 1
+	return count - 1
+
+def ishappynumber(n):
+	arr = []
+	if n < 0:
+		return False
+	while n != 1:
+		n = sum(int(i)**2 for i in str(n))
+		if n in arr:
+			return False
+		arr.append(n)
+	return True
+
+def isprimeNum(n):
+	if n > 1:
+		for i in range(2, n//2):
+			if n%i == 0:
+				return False
+				break
+		return True
+	else:
+		return False
+
 def fun_nth_happy_prime(n):
 	return 0
