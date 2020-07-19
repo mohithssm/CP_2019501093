58d6fbb5df7d68822c400a711dd27fb703504c95
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-19 13:26:09 +0530

    Jul 19  13:26:09

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -2,5 +2,22 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-	# Your code goes here
-	pass
+
+	sort(array, 0, len(array) - 1)
+
+def sort(array, low, high):
+	if low < high:
+		p = partition(array, low, high)
+		sort(array, low, p-1)
+		sort(array, p+1, high)
+
+def partition(array, low, high):
+	i = low - 1
+	pivot = array[high]
+
+	for j in range(low, high):
+		if array[j] <= pivot:
+			i = i + 1
+			array[i], array[j] = array[j], array[i] 
+	array[i+1], array[high] = array[high], array[i+1]
+	return (i+1)
