08f62cdc7ba175be1bf687faef75ea8a2800db33
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-19 23:35:02 +0530

    Jul 19  23:35:02

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -17,7 +17,7 @@ def fun_nth_palindromic_prime(n):
 
 def fun_is_prime(n):
 	if n > 1:
-		for i in range(n, (n//2) + 1):
+		for i in range(2, (n//2) + 1):
 			if n%i == 0:
 				return False
 				break
