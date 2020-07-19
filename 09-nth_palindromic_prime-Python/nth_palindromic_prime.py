3df3b66f8b3a3fe9c4b60651506a1de809fbe50e
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-19 23:24:24 +0530

    Jul 19  23:24:24

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -37,11 +37,9 @@ def is_palindrome(n):
 		return False
 
 
-print(fun_nth_palindromic_prime(0))
-print(fun_nth_palindromic_prime(1))
 print(fun_nth_palindromic_prime(10))
-print(fun_nth_palindromic_prime(15))
-print(fun_nth_palindromic_prime(20))
-print(fun_nth_palindromic_prime(25))
+# print(fun_nth_palindromic_prime(15))
+# print(fun_nth_palindromic_prime(20))
+# print(fun_nth_palindromic_prime(25))
 
 
