b06e4e707bb320adf555f08cd557baee837fc13e
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-19 23:32:25 +0530

    Jul 19  23:32:25

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -9,7 +9,7 @@
 def fun_nth_palindromic_prime(n):
 	count = 1
 	while n >= 0:
-		print(count)
+		# print(count)
 		if  is_palindrome(count) and fun_is_prime(count):
 			# print("inside", n, count)
 			n = n - 1
