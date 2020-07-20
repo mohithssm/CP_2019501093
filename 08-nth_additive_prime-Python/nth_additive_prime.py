e0a98de69cfb10ded9883ba7c259d02568d3c04c
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-20 16:45:22 +0530

    Jul 20  16:45:22

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -6,66 +6,33 @@
 
 
 
-# def fun_nth_additive_prime(n):
-# 	count = 1
-# 	while n >= 0:
-# 		if fun_is_prime(count) and addictive_number(count):
-# 			n = n - 1	
-# 		count = count + 1
-# 	return count - 1
+def fun_nth_additive_prime(n):
+	count = 1
+	while n >= 0:
+		if fun_is_prime(count) and addictive_number(count):
+			n = n - 1	
+		count = count + 1
+	return count - 1
 
 
-# def fun_is_prime(n):
-# 	if n > 1:
-# 		for i in range(2, (n//2)+1):
-# 			if n%i == 0:
-# 				return False
-# 				break
-# 		return True
-# 	else:
-# 		return False
-
-# def addictive_number(n):
-# 	# lst = []
-# 	tot = 0
-# 	while n > 0:
-# 		digit = n % 10
-# 		n = n // 10
-# 		tot = tot + digit
-# 		# lst.append(digit)
-# 	# total = sum(lst)
-	
-# 	if fun_is_prime(tot):
-# 		return True
-# 	else:
-# 		return False
-
-def isprime(n):
-		if n > 1:
-			for i in range(2,(n//2) + 1):
-				if n % i == 0:
-					return False
-			return True
-		else:
-			return False
-
-def isadditive(n):
-	su = 0
-	while n > 0:
-		rem = n % 10
-		su += rem
-		n = n // 10
-	if isprime(su):
+def fun_is_prime(n):
+	if n > 1:
+		for i in range(2, (n//2)+1):
+			if n%i == 0:
+				return False
+				break
 		return True
 	else:
 		return False
 
-def fun_nth_additive_prime(n):
-	var = 1
-	while n >= 0:
-		if isprime(var) and isadditive(var):
-			n -= 1
-		var += 1
-	return var - 1
-
-print(fun_nth_additive_prime(5))
+def addictive_number(n):
+	lst = []
+	while n > 0:
+		digit = n % 10
+		n = n // 10
+		lst.append(digit)
+	total = sum(lst)
+	if fun_is_prime(total):
+		return True
+	else:
+		return False
