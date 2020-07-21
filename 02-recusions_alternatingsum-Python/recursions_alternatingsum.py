e5c83b8a99f6fa44a010d04009a1f3fda5805e22
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-22 01:41:05 +0530

    Jul 22  01:41:05

--- 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
+++ 02-recusions_alternatingsum-Python/recursions_alternatingsum.py
@@ -8,7 +8,8 @@ def fun_recursions_alternatingsum(l):
 	if len(l) == 0:
 		return 0
 	else:
-		print(l[2::2])
+		print(l[::1])
+		print(l[::2])
 		return l[0] + fun_recursions_alternatingsum(l[2::2]) - fun_recursions_alternatingsum(l[1::1])
 
 print(fun_recursions_alternatingsum([5,3,7,8, 9, 11, 45]))
