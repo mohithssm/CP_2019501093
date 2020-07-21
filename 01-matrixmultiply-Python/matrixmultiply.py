d6e0bd794af2a4b8bf7983c5848e71af1555882c
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-22 01:22:39 +0530

    Jul 22  01:22:39

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -2,15 +2,19 @@
 # (that we will consider to be matrices) and returns a new 2d list that 
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
-import numpy as np
 
 def fun_matrixmultiply(m1, m2):
+    result = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    print(result)
     if len(m1[0]) == len(m2):      
-        result = (np.dot(m1, m2))
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                for k in range(len(m2)):
+                    result[i][j] += m1[i][k] * m2[k][j]
         print(result)
         return result
     else:
         return None
-print(fun_matrixmultiply([[1,3],[2,4],[2,5]], [[1,3,2,2], [2,4,5,1]]))
+print(fun_matrixmultiply([[12,7,3], [4,5,6], [7,8,9]], [[5,8,1,2], [6,7,3,0], [4,5,9,1]]))
 
 
