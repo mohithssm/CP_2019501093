b27bfb704cb59b60e72eb5403a6788905ed43c10
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-21 23:55:47 +0530

    Jul 21  23:55:47

--- 02-hasnoprimes-Python/test_hasnoprimes.py
+++ 02-hasnoprimes-Python/test_hasnoprimes.py
@@ -6,7 +6,7 @@ import pytest
 
 @pytest.mark.parametrize('s1, result',[	
 ([[12,4,6],[8,12,14],[6,18]], True), ([[2]], False),
-([[9,12],[8],[16,8]], True),([[9,12],[8],[16,8,19]],False) 
+([[9,12],[8],[16,8]], True),([[9,12],[8],[16,8,19]],True) 
 ])
 def test_fun_hasnoprimes(s1, result):
     assert fun_hasnoprimes(s1) == result
