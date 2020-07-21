b6b7a9d5b4d019ba7fa500ea9576b753029f7869
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-22 00:29:58 +0530

    Jul 22  00:29:58

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -2,11 +2,10 @@
 # (that we will consider to be matrices) and returns a new 2d list that 
 # is the result of multiplying the two matrices. Return None if the 
 # two matrices cannot be multiplied for any reason.
-
+import numpy as np
 
 def fun_matrixmultiply(m1, m2):
-    return None
-
-
+    result = m1.dot(m2)
+    return result
 
 
