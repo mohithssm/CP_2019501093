84b3f50e4c9a30efbff6b89951f29ba542043193
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-22 11:21:56 +0530

    Jul 22  11:21:56

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -9,4 +9,7 @@
 # Remember to not use strings. You may not use loops/iteration in this problem.
 
 def fun_recursion_onlyevendigits(l): 
-		return []
+		if l == []:
+			return []
+		else:
+			return fun_recursion_onlyevendigits[l:]
