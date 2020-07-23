3d9c0ca90d8add8a2f9679174bed49275c14fe4f
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-24 01:11:47 +0530

    Jul 24  01:11:47

--- 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
+++ 03-recursion_onlyevendigits-Python/recursion_onlyevendigits.py
@@ -36,7 +36,6 @@ def fun_checkeven(l):
 			rem = lst[j] % 10
 			rev2 = (rev2*10) + rem
 			lst[j] = lst[j] // 10
-			# print(rev2)
 		mainLst.append(rev2)
 	return mainLst
 print(fun_recursion_onlyevendigits([43, 23265, 17, 58344]))
