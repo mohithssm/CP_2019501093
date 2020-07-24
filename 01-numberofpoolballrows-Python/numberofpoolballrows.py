501c08db89573bd1b210fa039ae5a788f453c356
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-24 19:51:01 +0530

    Jul 24  19:51:01

--- 01-numberofpoolballrows-Python/numberofpoolballrows.py
+++ 01-numberofpoolballrows-Python/numberofpoolballrows.py
@@ -6,11 +6,8 @@
 # numberOfPoolBallRows(7) returns 4 (since the 4th row must have a single ball in it).
 
 def fun_numberofpoolballrows(balls):
-	if balls > 0:
-		a = 1
-		d = 1
-		res = (balls//2) *(2**a + (balls-1) * d)
-		return res
+	res = balls * (balls + 1) / 2
+	return res
 
 print(fun_numberofpoolballrows(36))
 
