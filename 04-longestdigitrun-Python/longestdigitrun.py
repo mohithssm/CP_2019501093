18988a7d41764c6c11cfc3a8f9294e7b248d55a3
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-25 19:25:30 +0530

    Jul 25  19:25:30

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -4,6 +4,24 @@
 # run, or the smallest such digit if there is a tie. So, longestDigitRun(117773732) returns 7 (
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
+
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+	digit_lst = n_arr(n)
+	size = 1
+	max_size = 0
+	for i in range(len(digit_lst) - 1):
+		if digit_lst[i+1] == digit_lst[i]:
+			size = size + 1
+		else:
+			size = 1
+		if max_size < size:
+			max_size = size
+	return size
+
+def n_arr(n):
+	digit_lst = []
+	while n > 0:
+		rem = n % 10
+		digit_lst.append(rem)
+		n = n // 10
+	return digit_lst[::-1]
