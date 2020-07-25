6073d3ff9d444b623ecb756790319960f93acd65
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-25 20:58:53 +0530

    Jul 25  20:58:53

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -11,7 +11,7 @@ def longestdigitrun(n):
 	lst = n_arr(n)
 	keyGroup = {}
 	for key, value in itertools.groupby(lst):
-		print(key, list(value))
+		print(key, len(list(value)))
 	return keyGroup
 def n_arr(n):
 	digit_lst = []
