a2298ed5cb99c3a7934a69f3524937e1aa87d80a
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-26 22:55:54 +0530

    Jul 26  22:55:54

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -3,10 +3,18 @@
 # number itself. For example, 5^2 = 25, 6^2 = 36, 76^2 = 5776, and 890625^2 = 793212890625, so 5, 6, 
 # 76 and 890625 are all automorphic numbers.
 
+import math
+
 def nthautomorphicnumbers(n):
     count = 1
     while n>= 0:
-        if isTidy(count):
+        if isAutomorphic(count):
             n = n - 1
         count = count + 1
     return count - 1
+
+def isAutomorphic(num):
+	if math.sqrt(num) == num**2:
+		return True
+	else:
+		return False
