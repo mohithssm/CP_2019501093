2b5063e4acc3be7fdfd2c885e2869a09b3a24fff
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-26 23:39:27 +0530

    Jul 26  23:39:27

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -15,8 +15,8 @@ def nthlychrelnumbers(n):
 
 def isLychrelNum(num):
 	for i in range(30):
-		num = num + int(str(num)[::-1])
-		if num == int(str(num)[::-1]):
+		res = num + int(str(num)[::-1])
+		if res == int(str(num)[::-1]):
 			return False
 	return True
 
