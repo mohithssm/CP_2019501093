f3e7d680ed40f944052a71d3968b51b4a118fd00
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-27 01:06:09 +0530

    Jul 27  01:06:09

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -9,14 +9,37 @@ class BST(object):
         self.root = Node(root)
 
     def insert(self, new_val):
-        # Your code goes here
-        pass
+        self.insert_helperfunc(self.root, new_val)
 
     def printSelf(self):
         # Your code goes here
         pass
         
     def search(self, find_val):
+        # Your code goes here
+        pass
+
+
+    def insert_helperfunc(self, new_val):
+        if root is None:
+            root = Node(new_val)
+        else:
+            if root.value < new_val:
+                if root.right is None:
+                    root.right = Node(new_val)
+                else:
+                    self.insert_helperfunc(root.right, new_val)
+            else:
+                if root.left is None:
+                    root.left = Node(new_val)
+                else:
+                    self.insert_helperfunc(root.left, new_val)
+
+    def printSelf_helperfunc(self):
+        # Your code goes here
+        pass
+        
+    def search_helperfunc(self, find_val):
         # Your code goes here
         pass
 
