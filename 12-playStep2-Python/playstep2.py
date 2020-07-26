ed288231fc356557bd225e6ab03e770ed4825eda
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-27 00:41:39 +0530

    Jul 27  00:41:38

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -45,6 +45,7 @@ def playstep2(hand, dice):
 		dice = dice // 10
 	hand = diceTo(x, y, z)
 	return hand, dice
+
 def handToDice(hand):
 	st = str(hand)
 	return int(st[0]), int(st[1]), int(st[2])
@@ -52,6 +53,6 @@ def handToDice(hand):
 def diceTo(x, y, z):
 	max_i = max(x, y, z)
 	min_i = min(x, y, z)
-	mid = (x + y + z) - (max_i - min_i)
+	mid = (x + y + z) - (max_i + min_i)
 	res = (max_i * 100) + (mid * 10) + min_i
 
