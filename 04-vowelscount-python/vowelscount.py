781c76afca2266a414f38b2f7c71f49ab576f904
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-27 23:23:17 +0530

    Jul 27  23:23:16

--- 04-vowelscount-python/vowelscount.py
+++ 04-vowelscount-python/vowelscount.py
@@ -1,8 +1,8 @@
-def vowelscount(S):
+def vowelscount(n):
     arr = []
     count = 0
     owels = "aeiou"
-    for elem in S:
+    for elem in n:
         if elem in owels:
             count = count + 1
     return count
