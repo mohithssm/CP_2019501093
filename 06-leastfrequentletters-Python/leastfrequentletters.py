ec2224177735fa466acb1066ab136e54308c7c1a
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-28 17:42:06 +0530

    Jul 28  17:42:06

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -25,5 +25,5 @@ def leastfrequentletters(s):
 	for elem in lfl:
 		if elem in freqletters:
 			lfl.remove(elem)
-	return "".join(lfl)
+	return "".join(lfl[:-1])
 print(leastfrequentletters("aDq efQ? FB'daf!!!"))
