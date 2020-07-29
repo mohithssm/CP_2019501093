ad272208dbf86c694ed4d63387d0400753292b0c
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-30 00:31:00 +0530

    Jul 30  00:31:00

--- 02-hasbalancedparanthesis-python/hasbalancedparantheses.py
+++ 02-hasbalancedparanthesis-python/hasbalancedparantheses.py
@@ -1,2 +1,9 @@
 def fun_hasbalancedparantheses(a):
-    pass 
+    count = 0
+    for i in range(len(a)):
+        if a.charAt(i) == '(':
+            count = count + 2
+        elif a.charAt(9) == ')':
+            count = count - 1
+    
+    return count == 0
