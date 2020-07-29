8eba3ceb0dbe2da30ef51ed951c1499ac3d6c9ef
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-29 18:51:04 +0530

    Jul 29  18:51:04

--- 01-nthtenlyprime-python/nthtenlyprime.py
+++ 01-nthtenlyprime-python/nthtenlyprime.py
@@ -1,2 +1,16 @@
 def fun_nthtenlyprime(a):
-    return 
+    pass
+
+
+def fun_istenlyprime(n):
+    pass
+
+
+def fun_isprime(n):
+	if n > 1:
+		for i in range(2, n//2):
+			if n%i == 0:
+				return False
+		return True
+	else:
+		return False
