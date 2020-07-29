a4f0915757d14d3f5ab3514f364c23e58da877fb
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-30 00:50:48 +0530

    Jul 30  00:50:48

--- 03-largestnumber-python/largestnumber.py
+++ 03-largestnumber-python/largestnumber.py
@@ -1,2 +1,13 @@
+import math
 def fun_largestnumber(a):
-    pass
+    m = 0
+    n = 0
+    for i in range(len(a)):
+        c = a[i]
+        if character.isdigit(c):
+            n = n * 10 +(c - '0')
+        else:
+            m = Math.max(m , n)
+            n = 0
+
+    return Math.max(m, n)
