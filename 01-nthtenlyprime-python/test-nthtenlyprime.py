b99a874cbc90de903a6bfd8a9752f9ebe47a7090
mohithssm <mohithsekharamantri@msitprogram.net>
2020-07-29 14:38:33 +0530

    Jul 29  14:38:32

--- 01-nthtenlyprime-python/test-nthtenlyprime.py
+++ 01-nthtenlyprime-python/test-nthtenlyprime.py
@@ -7,5 +7,5 @@ import pytest
 @pytest.mark.parametrize('a, result',[
     (0,19),(1, 37),(4, 127), (10, 523), (15, 1009),  (20, 1423),  (25, 2053)
 ])
-def test_fun_nthtenlyprime(a, result):
+def test_nthtenlyprime(a, result):
     assert fun_nthtenlyprime(a) == result
